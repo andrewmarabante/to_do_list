38722d0d7fadde4d1771bc9be57c3eb92cac04b9
Andrew Marabante <andrewmarabante@gmail.com>
2023-08-07 23:58:57 -0700

    Project Reset: I deleted everything and outlined my goals as I feel things got a bit convoluted and I wasn't accomplishing what I wanted.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
-import { loadPage, newTask } from "./dom";
-loadPage();
+//Main Goal: Create a to-do list
+
+/*
+Goal 1:
+ Create a task factory that takes the values 
+ 'title, description, dueDate, and priority'
+ and stores the objects in a global array
+*/
+
+/*
+Goal 2: Create a project factory that takes the
+project name and stores the objects in a global
+array 
+*/
+
+/*
+Goal 3:
+Make add/remove buttons for tasks and projects.
+*/
+
+/*
+Goal 4:
+Create a user interface that allows a home to
+view all projects, view one specific project,
+and go back to all projects.
+*/
+
+/*
+Goal 5:
+Make everything look pretty!
+*/
